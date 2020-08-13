dc8be608c9905ed291a8147fc39290d37871f33c
shjiang-yang <49501818+shjiang-yang@users.noreply.github.com>
2020-08-13 15:26:26 +0800

    Update config.v

--- SDRAM/src/config.v
+++ SDRAM/src/config.v
@@ -10,7 +10,7 @@
 // -------------- async FIFO ------------------------
 `define FIFO_WIDTH      16
 `define FIFO_DEPTH      512
-`define POINTER_WIDTH   10
+`define POINTER_WIDTH   10 
 
 
 // -------------- sdram config -----------------------
@@ -21,5 +21,4 @@
 // bank (one-hot), can be {PINGPONG_BUFFER, BANK_INCR, BANK_ONE}
 // for PINGPONG_BUFFER, assume that read port faster than write port
 `define     PINGPONG_BUFFER
-
 
